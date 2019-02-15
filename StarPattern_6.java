168a6f0a56fb7be3d2ddff36afbd378c9bb3fef2
SHIHAB KHAN <41753677+shihab0005@users.noreply.github.com>
2019-02-15 12:24:09 -0800

    Add files via upload

---
+++ StarPattern_6.java
@@ -0,0 +1,51 @@
+
+import java.util.Scanner;
+
+/*
+Output -
+    #
+   ##
+  ###
+ ####
+#####
+ ####
+  ###
+   ##
+    #
+ */
+
+/**
+ *
+ * @author SK
+ */
+public class StarPattern_6 {
+    public static void main(String[] args) {
+       
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Number Of Row You Want to Print : ");
+        int a = input.nextInt();
+         for (int i = 1; i <= a; i++) {
+            
+            for (int j = a-1; j>= i; j--) {
+                System.out.print(" ");
+                
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("#");
+                
+            }
+            System.out.println();
+            
+        }
+         for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = a-1; k >= i; k--) {
+                System.out.print("#");
+
+            }
+            System.out.println();
+        }
+    }
+}
